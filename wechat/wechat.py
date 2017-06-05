0b87f788fe71565704d3c23f731c2bb0627ff422
Aaron Chen <aaron.nehc@gmail.com>
2017-06-05 22:38:32 +0800

    Add WeChatException class.
    Signed-off-by: Aaron Chen <aaron.nehc@gmail.com>

--- wechat/wechat.py
+++ wechat/wechat.py
@@ -4,12 +4,16 @@ import requests
 from flask import current_app
 
 
+class WeChatException(Exception):
+    pass
+
+
 class WeChatBase(object):
 
     WX_TOKEN_API_URL = "https://api.weixin.qq.com/cgi-bin/token"
 
 
-class WeChatHelper():
+class WeChatHelper(WeChatBase):
 
     def get_access_token(self):
         url = "{0}?grant_type=client_credential&appid={1}&secret={2}".format(
@@ -17,9 +21,10 @@ class WeChatHelper():
             current_app.config['APPID'],
             current_app.config['APPSECRET']
         )
-        print url
         resp = requests.get(url).json()
-        print resp
+        if "errcode" in resp.keys():
+            raise WeChatException(resp['errmsg'])
+        return resp['access_token']
 
     def send_request(self, url, data):
         pass
